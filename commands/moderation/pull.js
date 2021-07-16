22ebc7f6e938bd2b72ab5293db366898f9b498a9
BuildTools <unconfigured@null.spigotmc.org>
2021-07-15 20:18:25 -0400

    error check

--- commands/moderation/pull.js
+++ commands/moderation/pull.js
@@ -14,6 +14,7 @@ module.exports = {
         await message.reply("Pulling git changes and restarting bot.");
         client.guilds.cache.get(config.AC).channels.cache.get(config.logs).send("Pulling changes...");
         git().pull('origin', 'master', {}, async (err, result) => {
+            if(err) return console.log(err);
             if (result.summary.changes === 0 && result.summary.insertions === 0 && result.summary.deletions === 0) {
                 await client.guilds.cache.get(config.AC).channels.cache.get(config.logs).send("The bot is up to date.");
             } else {
