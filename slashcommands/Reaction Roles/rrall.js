d1d36a2f6dcde04eee433b8eb0c37d4910d911c0
RiverRipple <51027259+RiverRipple@users.noreply.github.com>
2021-09-17 12:18:15 -0400

    eggs hehe

--- slashcommands/Reaction Roles/rrall.js
+++ slashcommands/Reaction Roles/rrall.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const config = require('../../config.json');
-const emojis = require('../../emojis.js');
 const rrSchema = require('../../models/rrschema.js');
 module.exports = {
 	name: 'rrall',
@@ -13,13 +12,13 @@ module.exports = {
 		const fields = [];
 		for (let i = 1;i < numRRs + 1; i++) {
 			const schema = await rrSchema.findOne({ id: i });
-			const msgchannel = await message.guild.channels.cache.get(schema.channelID);
+			const msgchannel = await interaction.guild.channels.cache.get(schema.channelID);
 			const msg = await msgchannel.messages.fetch(schema.messageID);
 			fields.push({ 'name': `ID: #${i}`, 'value': `[Jump to ${msgchannel.toString()}](${msg.url})` });
 		}
 		const embed = new Discord.MessageEmbed()
 			.setColor(config.embedColor)
-			.setTitle('Reaction Roles for ' + message.guild.name)
+			.setTitle('Reaction Roles for ' + interaction.guild.name)
 			.addFields(fields);
 		return interaction.editReply({ embeds: [embed] });
 	},
