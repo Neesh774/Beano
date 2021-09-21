fb261d2b13b2c25cfa197a589b94f2a72ed2f2e9
RiverRipple <51027259+RiverRipple@users.noreply.github.com>
2021-09-20 22:09:42 -0400

    another join fix

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,29 +2,20 @@ const Discord = require('discord.js');
 const config = require('../config.json');
 const mSchema = require('../models/memberschema.js');
 const welcomes = require('../welcomes.json');
-
+const databaseFuncs = require('../functions/databaseFuncs');
 module.exports = {
 	name: 'guildMemberAdd',
 	async execute(member, client) {
 		if (member.guild.id != config.AC) return;
 		const AC = await client.guilds.fetch(config.AC);
-		const ms = new mSchema({
-			rank: AC.memberCount + 1,
-			name: member.nickname,
-			userID: member.id,
-			coolDown: false,
-			muted: 0,
-			starboards: 0,
-		});
-		await ms.save();
+		await databaseFuncs.createMember(member.user.username, member.user.id);
 
 		const ranInt = Math.floor(Math.random() * welcomes.length);
-		console.log(ranInt);
 		let message = welcomes[ranInt];
 		message = message.replace('NAME', member.user.toString());
 		message = message.replace('COUNT', member.guild.memberCount);
 		const general = await member.guild.channels.fetch(config.general);
-		// general.send(message);
+		general.send(message);
 
 		const logs = await AC.channels.cache.get(config.logs);
 		const embed = new Discord.MessageEmbed()
